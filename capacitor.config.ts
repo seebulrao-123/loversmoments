9eed9a4ee9075fc2eaee183aa3c652ab654ccb69
seebulrao-123 <seebulrao@gmail.com>
2026-09-07 22:52:35 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.moments.diary',
+  appName: 'Moments..',
+  webDir: 'dist'
+};
+
+export default config;
